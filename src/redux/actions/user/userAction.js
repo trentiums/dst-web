0534b2196e7b9afdf9b65a901f86e5ec4198d9cc
Nidhi Gandhi <nidhi@trentiums.com>
2021-01-12 15:24:35 +0530

    removed unnecessary code to fix redux bug.

--- src/redux/actions/user/userAction.js
+++ src/redux/actions/user/userAction.js
@@ -1,13 +1,6 @@
 import * as userTypes from './userTypes'
 import Api, { urls, setCookie, setAuthorization } from '../../../services/api'
 import { auth } from '../../../services/firebase'
-// import DeviceInfo from '../../../Services/DeviceInfo'
-import * as keysAction from '../keys/keysAction'
-import * as notificationAction from '../notification/notificationAction'
-// import { showMessage } from '../../../heplers/messagebar'
-// import Config from '../../../Services/Config'
-import * as teamSpaceTypes from '../teamSpace/teamSpaceTypes'
-
 export const userUpdateSuccess = (response) => {
   return {
     type: userTypes.USER_UPDATE_SUCCESS,
@@ -83,49 +76,6 @@ export const userLogout = () => async (dispatch) => {
     type: userTypes.USER_LOGOUT,
   })
 }
-export const loginwithCookieOrCreateGuest = (properties) => async (dispatch, getState) => {
-  try {
-    const storedCookie = getState().keys.storedcookie
-    console.log('cookie in State is' + storedCookie)
-    properties = {
-      ...properties,
-      // deviceId: DeviceInfo.hostDevice.getUniqueId(),
-      storedCookie: getState().keys.storedcookie,
-      // deviceInfo: DeviceInfo.deviceProperties,
-    }
-    // properties.deviceInfo.deviceName = await DeviceInfo.hostDevice.getDeviceName()
-    // properties.deviceInfo.userAgent = await DeviceInfo.hostDevice.getUserAgent()
-    // properties.deviceInfo.Manufacturer = await DeviceInfo.hostDevice.getManufacturer()
-    const responseJson = await Api.post(urls.userLogin, properties)
-    // currently username and password are equal
-    dispatch(userLoginSuccess(responseJson.data.userDevice))
-    dispatch({
-      type: teamSpaceTypes.FETCH_USER_TEAMSPACE_SUCCESS,
-      payload: responseJson.data.teamSpaceList,
-    })
-    dispatch(
-      setApiCredentials(
-        responseJson.data.userDevice.user.username,
-        responseJson.data.userDevice.user.username,
-      ),
-    )
-    await dispatch(keysAction.storeCookieStart(responseJson.data.userDevice.cookie))
-    if (responseJson.data?.backendMessages.length) {
-      dispatch(
-        notificationAction.updateNotification({
-          showNotification: true,
-          messages: responseJson.data.backendMessages,
-        }),
-      )
-    }
-    return responseJson.data.nextActions
-  } catch (e) {
-    console.info('checkCookie api' + e)
-    // Server error not handled
-    await dispatch(keysAction.clearCookieStart())
-    await dispatch(loginwithCookieOrCreateGuest())
-  }
-}
 
 export const setApiCredentials = (username, password) => () => {
   setAuthorization({ username, password })
@@ -135,32 +85,6 @@ export const userLoginSuccess = (response) => {
   return {
     type: userTypes.USER_LOGIN_SUCCESS,
     payload: response,
-  }
-}
-
-export const licenseUpdated = (newLicenseCount) => {
-  return {
-    type: userTypes.LICENSE_UPDATED,
-    payload: newLicenseCount,
-  }
-}
-
-export const getLicense = () => async (dispatch, getState) => {
-  try {
-    // depends on proper authorization, which currently doesn't exist
-    const { teamSpaceUid } = getState().teamSpace
-    if (!teamSpaceUid) {
-      return Promise.resolve()
-    }
-    await Api.get(urls.getLicense.format(teamSpaceUid)).then((res) => {
-      dispatch(licenseUpdated(res.data.length))
-    })
-  } catch (e) {
-    console.log(e)
-    // showMessage({
-    //   type: Config.INFORMATION_LEVEL['ERROR'],
-    //   text: 'Server error. Could not get license information',
-    // })
   }
 }
 
@@ -181,23 +105,4 @@ export const getUserProfile = () => async (dispatch, getState) => {
       console.log(e)
     }
   }
-}
-
-export const refreshUserLayout = (payload) => async (dispatch) => {
-  dispatch({
-    type: userTypes.USER_TYPE,
-    payload: payload,
-  })
-}
-
-export const resetLayoutFor = () => {
-  return {
-    type: userTypes.RESET_LAYOUT,
-    payload: {},
-  }
-}
-
-export const fetchCurrentUser = () => async (dispatch) => {
-  const responseJson = await Api.post(urls.checkCookie)
-  dispatch(userLoginSuccess(responseJson.data))
 }
