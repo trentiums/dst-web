0bcf9cd8d784df71b3dffe3bd8a2d626ef390b17
Nidhi Gandhi <nidhi@trentiums.com>
2021-02-04 11:25:43 +0530

    Date Picker issue fixed.

--- src/components/commonModals/calendarModal.js
+++ src/components/commonModals/calendarModal.js
@@ -1,66 +1,77 @@
-import React, { memo,useState } from 'react'
+import React, { memo, useState } from 'react'
 import { Button, Modal } from 'react-bootstrap'
-import images from '../../assets/images'
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
 
 function CalendarModal({ isShow, setShow }) {
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState();
-    const onDateChange = (dates) => {
-        // console.log(Moment(date[0]).format("YYYY/MM/DD"))
-        const [start, end] = dates;
-        setStartDate(start);
-        setEndDate(end);
-        console.log(startDate,"ssss",endDate)
-    }
-     const onDateSelect = (dates,endt) => {
-            console.log(endt)
-            const [start, end] = dates;
-            setStartDate(start);
-            setEndDate(end);
-            console.log(startDate,"ssss",endDate)
-    
-        }
-    return (
-        <div>
-            <Modal show={isShow} onHide={() => setShow(false)}>
-                <Modal.Header closeButton style={{ display: "flex",justifyContent:"center" }}>
-                    <Modal.Title> SELECT DATE RANGE</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <form className="form-signin " onSubmit={() => { }}>
-                        <div className="row">
-                            <div className="col-md-12" style={{ display: "flex",justifyContent:"center" }}>
-                                <DatePicker
-                                        maxDate={new Date()}
-                                        selected={startDate}
-                                        dateFormat="dd/MM/yyyy"
-                                        selected={startDate}
-                                        // onSelect={onDateSelect}
-                                        onChange={onDateChange}
-                                        startDate={startDate}
-                                        endDate={endDate}
-                                        selectsRange
-                                        inline
-                                    />
-                            </div>
-                        </div>
-                    </form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={() => setShow(false)}>
-                        Close
+  const [startDate, setStartDate] = useState()
+  const [endDate, setEndDate] = useState()
+  const [error, setError] = useState(null)
+  const onDateChange = (dates) => {
+    const [start, end] = dates
+    setStartDate(start)
+    setEndDate(end)
+  }
+  return (
+    <div>
+      <Modal
+        show={isShow}
+        onHide={() => {
+          setError(null)
+          setShow(false)
+        }}
+      >
+        <Modal.Header closeButton style={{ display: 'flex', justifyContent: 'center' }}>
+          <Modal.Title> SELECT DATE RANGE</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form className="form-signin " onSubmit={() => {}}>
+            {error && <div className="text-center error-msg mb-3">{error}</div>}
+            <div className="row">
+              <div className="col-md-12" style={{ display: 'flex', justifyContent: 'center' }}>
+                <DatePicker
+                  maxDate={new Date()}
+                  dateFormat="dd/MM/yyyy"
+                  onChange={onDateChange}
+                  startDate={startDate}
+                  endDate={endDate}
+                  selectsRange
+                  shouldCloseOnSelect={startDate && endDate}
+                  inline
+                />
+              </div>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={() => {
+              setError(null)
+              setShow(false)
+            }}
+          >
+            Close
           </Button>
-                    <Button
-                        style={{ backgroundColor: '#6dbd8e', borderColor: '#7dce9f' }}
-                        onClick={() => setShow(false)}
-                    >
-                        Confirm
+          <Button
+            style={{ backgroundColor: '#6dbd8e', borderColor: '#7dce9f' }}
+            onClick={() => {
+              console.log(startDate, endDate)
+              if (!startDate) {
+                setError('Please select start date.')
+              } else if (!endDate) {
+                setError('Please select end date.')
+              } else {
+                setError(null)
+                setShow(false)
+              }
+            }}
+          >
+            Confirm
           </Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    )
+        </Modal.Footer>
+      </Modal>
+    </div>
+  )
 }
 export default memo(CalendarModal)
